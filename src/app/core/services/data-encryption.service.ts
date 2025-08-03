ecb1ffeda5a9f46057c334e175608de7b2e33a27
İbrahim Çelik <clkibrahim@outlook.com>
2025-08-03 16:30:37 +0300

    format öncesi commit

--- src/app/core/services/data-encryption.service.ts
+++ src/app/core/services/data-encryption.service.ts
@@ -1,4 +1,4 @@
-const CryptoJS = require('crypto-js');
+import * as CryptoJS from 'crypto-js';
 import { Injectable } from '@angular/core';
 import { environment } from './../../../environments/environment';
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,27 +1,14 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
-
+// Otomatik oluşturuldu
 export const environment = {
   production: false,
   firebase: {
-    apiKey: process.env['FIREBASE_API_KEY'] || '',
-    authDomain: process.env['FIREBASE_AUTH_DOMAIN'] || '',
-    projectId: process.env['FIREBASE_PROJECT_ID'] || '',
-    storageBucket: process.env['FIREBASE_STORAGE_BUCKET'] || '',
-    messagingSenderId: process.env['FIREBASE_MESSAGING_SENDER_ID'] || '',
-    appId: process.env['FIREBASE_APP_ID'] || '',
-    measurementId: process.env['FIREBASE_MEASUREMENT_ID'] || '',
+    apiKey: 'AIzaSyBcEBmy_OTAMysbK6eHVFdszuwk7lyssZA',
+    authDomain: 'okidoki-b4ec9.firebaseapp.com',
+    projectId: 'okidoki-b4ec9',
+    storageBucket: 'okidoki-b4ec9.firebasestorage.app',
+    messagingSenderId: '298161780303',
+    appId: '1:298161780303:web:5dacea957f4c801cc91c3a',
+    measurementId: 'G-532TK8XFQH',
   },
-  encryptionSecretKey: process.env['ENCRYPTION_SECRET_KEY'] || '',
+  encryptionSecretKey: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@',
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
